37386dae1aee403336622a796cc17b8d43ae7e1f
Srijoy Paul <srijoykumar1507@gmail.com>
2023-01-22 00:47:38 +0530

    Previw, Clear, Text summary func
    Signed-off-by: Srijoy Paul <srijoykumar1507@gmail.com>

--- src/InputText.js
+++ src/InputText.js
@@ -1,19 +1,41 @@
 import React, { useState } from "react";
 export default function InputText() {
     const [text, setText] = useState("");
+    const [preview, setPreview] = useState("");
     const handleClick = () => {
         const convertInUpperCase = text.toUpperCase();
         text != "" ? setText(convertInUpperCase) : alert("Input Box is empty")
+    }
+    const handleClickPreview = () => {
+        text != "" ? setPreview(text) : alert("Input Box is empty")
+    }
+    const handleClickClear = () => {
+        text != "" ? setText("") : alert("Input Box is empty")
     }
     const handleChange = (e) => {
         setText(e.target.value);
     }
     return (
-        <div className="container">
-            <h1>Enter your text here: </h1>
-            <div className="mb-3">
-                <textarea onChange={handleChange} value={text} className="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+        <>
+            <div className="container">
+                <h1>Enter your text here: </h1>
+                <div className="mb-3">
+                    <textarea onChange={handleChange} value={text} className="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                </div>
+                <button className='btn btn-success me-2' onClick={handleClick}>Convert Uppercase</button>
+                <button className='btn btn-success me-2' onClick={handleClickPreview}>Preview</button>
+                <button className='btn btn-danger' onClick={handleClickClear}>Clear Box</button>
             </div>
-            <button className='btn btn-primary' onClick={handleClick}>Convert Uppercase</button>
-        </div>);
+            <div className="container mt-3">
+                <h2>Your text summary</h2>
+                <p><span>{text != "" ? text.split(" ").length : 0}</span> Words, <span>{text.length}</span> Characters.</p>
+                <p>{text != "" ? (0.008 * (text.split(" ").length)).toFixed(0) : 0} minutes read</p>
+                <p></p>
+            </div>
+            <div className="container preview d-flex flex-column position-relative">
+                <h2>Text Preview</h2>
+                <button className="btn btn-success align-self-end position-absolute">Copy</button>
+                <p className="shadow-sm p-3 mb-5 bg-body rounded">{preview}</p>
+            </div>
+        </>);
 }
